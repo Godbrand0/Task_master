86e8be8922ee53f68efdcde1c24b8958aa6c1812
Thompson <140930314+Godbrand0@users.noreply.github.com>
2025-11-09 17:10:33 -0500

    updated

--- src/contracts/taskmaster.ts
+++ src/contracts/taskmaster.ts
@@ -1,9 +1,9 @@
-import * as Client from 'taskmaster';
-import { rpcUrl } from './util';
+import * as Client from "./src/index";
+import { rpcUrl } from "./util.ts";
 
 export default new Client.Client({
-  networkPassphrase: 'Standalone Network ; February 2017',
-  contractId: 'CD4JTVBF326WSZZ7RXQZTVUM6T5XKOMWPAKBBEFVJCXXYVBTHRZPGO6A',
+  networkPassphrase: "Standalone Network ; February 2017",
+  contractId: "CD4JTVBF326WSZZ7RXQZTVUM6T5XKOMWPAKBBEFVJCXXYVBTHRZPGO6A",
   rpcUrl,
   allowHttp: true,
   publicKey: undefined,
